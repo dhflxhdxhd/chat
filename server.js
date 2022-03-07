64d23a12d4a2ae785c023317097ae4a262b2b361
김아현 <dhflxhdxhd@gmail.com>
2022-03-07 23:44:02 +0900

    add app.js using websocket.io

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const wss = new WebSocket.Server({port:3000})
 
 // 클라이언트가 connection
 wss.on('connection',ws => {
+    
     // 메세지 받은 경우 호출
     ws.on('message',message =>{
         console.log('received: %s',message)
