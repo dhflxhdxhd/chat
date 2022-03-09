eb94450e837425a7211892d1f24715ef6f83111a
김아현 <dhflxhdxhd@gmail.com>
2022-03-09 19:59:18 +0900

    modify app.js
    http 서버 생성
    Use Express module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// 1) http 서버 생성(Express 모듈 사용)
 const express = require("express")
 const app = express();
 const path = require("path")
 
 app.use(express.static(path.join(__dirname,"src")))
 
+
 const PORT= process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`server is running ${PORT}`))
+const HTTPServer = app.listen(PORT,()=>console.log(`server is running ${PORT}`))
