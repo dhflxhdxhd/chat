7fcfc1951b09224f013f3d5378bea56c4a9267d3
김아현 <dhflxhdxhd@gmail.com>
2022-03-10 10:24:27 +0900

    fix(connection):client function event
    ws.on -> ws.addEventlistener

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const path = require("path")
 
 app.use(express.static(path.join(__dirname,"src")))
 
-
-const PORT= process.env.PORT || 5000;
-
+const PORT= process.env.PORT || 30001;
 const HTTPServer = app.listen(PORT,()=>console.log(`server is running ${PORT}`))
 
 // 2) 웹소켓 서버 생성
@@ -17,13 +15,10 @@ const WebSocket = require('ws')
 // port는 별도로 지정 가능
 const wss = new WebSocket.Server({
     server : HTTPServer,
-    // port 생략 시 http 서버와 동일 포트 사용
-    // ???????다른 포트를 사용하는 이유는 뭐지..????
-    // port : 3000 
 })
 
 // 클라이언트가 connection
-wss.on('connection',ws => {
+wss.on('connection', ws => {
 
     if(ws.readyState === ws.OPEN){
         ws.send(`클라이언트 접속을 환영합니다. from wss`)
@@ -31,15 +26,15 @@ wss.on('connection',ws => {
 
     // 메세지 받은 경우 호출
     ws.on('message',message =>{
-        console.log('received: %s',message)
+        console.log('received: %s from wss',message)
     })
 
     ws.on('error',(error) => {
-        console.log('연결 에러 발생')
+        console.log('연결 에러 발생 from wss')
     })
 
     ws.on('close', () =>{
-        console.log(`클라이언트 웹소켓 연결 종료`)
+        console.log(`클라이언트 웹소켓 연결 종료 from wss`)
     })
 
     ws.send('something')
